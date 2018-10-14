4b243137fb5a77806285ed5b1a601c701e215b51
Bartek Łukasik <Nexilis@users.noreply.github.com>
2018-10-14 16:52:46 +0200

    Added simple API based on httpkit/compojure.

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject questionnaire-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :description "RESTful API for a Questionnaire"
+  :url "https://github.com/Nexilis/questionnaire-api"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.1"]
+                 [http-kit "2.3.0"]
+                 [proto-repl "0.3.1"]])
--- src/questionnaire_api/core.clj
+++ src/questionnaire_api/core.clj
@@ -1,6 +1,11 @@
-(ns questionnaire-api.core)
+(ns questionnaire-api.core
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [org.httpkit.server :refer [run-server]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes myapp
+ (GET "/" [] "<h1>Hello World!</h1>")
+ (route/not-found "<h1>Page not found</h1>"))
+
+(defn -main [& args]
+ (run-server myapp {:port 5000}))
