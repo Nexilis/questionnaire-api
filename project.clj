d2d056cfe6740fdc0e552c1c9e8d1d31e5a1552b
Bartek Łukasik <Nexilis@users.noreply.github.com>
2018-10-20 09:14:10 +0200

    Changed to clojure 1.9

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
  (defproject blu-lama.api "0.1.0-SNAPSHOT"
-   :description "FIXME: write description"
-   :dependencies [[org.clojure/clojure "1.8.0"]
-                  [metosin/compojure-api "1.1.11"]]
+   :description "RESTful API for a Questionnaire"
+   :dependencies [[org.clojure/clojure "1.9.0"]
+                  [metosin/compojure-api "1.1.11"]
+                  [proto-repl "0.3.1"]]
    :ring {:handler blu-lama.api.handler/app}
    :uberjar-name "server.jar"
    :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
-                                  [cheshire "5.5.0"]
-                                  [ring/ring-mock "0.3.0"]
-                                  [midje "1.8.3"]]
-                   :plugins [[lein-ring "0.12.0"]
-                             [lein-midje "3.2"]]}})
+                                   [cheshire "5.5.0"]
+                                   [ring/ring-mock "0.3.0"]
+                                   [midje "1.8.3"]]
+                    :plugins [[lein-ring "0.12.0"]
+                              [lein-midje "3.2"]]}})
